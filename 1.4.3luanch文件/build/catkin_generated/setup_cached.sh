0a2118658cb9c0e5a4e5d7eb6abebb2385f65749
huxiaoshuo <hujiashuo2004@163.com>
2026-03-01 00:51:47 +0800

    launch 乌龟生成

---
+++ 1.4.3luanch文件/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/hujiashuo/毕业设计/ROS-study/1.4.3luanch文件/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/hujiashuo/毕业设计/ROS-study/1.4.3luanch文件/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/hujiashuo/毕业设计/ROS-study/1.4.3luanch文件/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/hujiashuo/毕业设计/ROS-study/1.4.3luanch文件/src:$ROS_PACKAGE_PATH"
