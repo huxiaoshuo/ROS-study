a5ca66ea8b6aefc8ea2ad929111027770a833972
huxiaoshuo <hujiashuo2004@163.com>
2026-03-03 20:01:56 +0800

    2026-03-03-002发布逻辑实现

--- 2.1.1/src/send_msg_211/src/send_msg_211.cpp
+++ 2.1.1/src/send_msg_211/src/send_msg_211.cpp
@@ -1,24 +1,38 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-
+#include <sstream>
 
 
 int main(int argc ,char *argv[])
 {
+    setlocale(LC_ALL,"");
     //初始化节点
     ros::init(argc,argv,"send");
-
+    //创建节点句柄
     ros::NodeHandle nh;
-
-    ros::Publisher pub = nh.advertise<std_msgs::String>("try_211",20);
-
+    //创建发布者对象
+    ros::Publisher pub = nh.advertise<std_msgs::String>("try_211",10);
+    //发布数据与发布逻辑
     std_msgs::String msg;
-
+    //1hz
+    ros::Rate rate(1);
+    //设置编号
+    int count = 0;
     while (ros::ok())
     {
-        msg.data = "hello";
-        pub.publish(msg);
+        count ++;
+        //msg.data = "hello";
+        //发送信息拼接
 
+        std::stringstream ss;
+                
+        ss << "hello  --->" << count;
+        msg.data = ss.str();
+        pub.publish(msg);
+        rate.sleep();
+
+        //添加日至
+        ROS_INFO ("发布的数据是:%s",ss.str().c_str());
     }
 
     return 0;
